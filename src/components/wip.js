bfe6c1571b472fc037269734f0bfad684281330b
Rohan Shah Summers <rohan_shah_summers@yahoo.co.uk>
2019-06-12 18:51:29 +0100

    added exit button to wip banner

--- src/components/wip.js
+++ src/components/wip.js
@@ -8,12 +8,33 @@ const Container = styled.section`
   font-size: 1em;
   text-align: center;
   padding: 0.5em 0;
+  display: ${props => props.display};
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const Exit = styled.button`
+  font-weight: bold;
+  color: white;
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  font-size: 1.5em;
+  position: absolute;
+  right: 0;
+  margin-right: 1em;
 `;
 
 const Wip = () => {
+
+  const [display, setDisplay]= React.useState('flex');
+
   return (
-    <Container>
+    <Container display={display}>
       This Portfolio is currently a work in Progress
+      <Exit onClick={() => setDisplay('none')}>X</Exit>
     </Container>
   )
 }
