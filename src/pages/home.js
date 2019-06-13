1c4efa79dc56c60bbf4dbf65873c95c5a1d9b037
Rohan Shah Summers <rohan_shah_summers@yahoo.co.uk>
2019-06-13 15:13:41 +0100

    updated cv

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import profilePic from '../assets/profile.jpg';
-import cv from '../assets/CV.pdf'
+import cv from '../assets/CV.pdf';
 
 const Container = styled.section`
   width: 100vw;
